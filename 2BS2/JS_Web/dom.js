71d4131b345bfa980b214831d8f7b8febaa6e4ad
Jan Mottl <mottja@delta-skola.cz>
2024-04-09 13:30:35 +0200

    kk

--- 2BS2/JS_Web/dom.js
+++ 2BS2/JS_Web/dom.js
@@ -19,3 +19,12 @@ console.log(selekce1);
 let selekce2 = document.querySelectorAll('main p');
 console.log(selekce2);
 selekce2[1].style = "color: red";
+
+//Manipulace s atributy DOM uzlů
+let obrazek = document.querySelector('img');
+obrazek.src = "https://d15-a.sdn.cz/d_15/c_img_F_E/6blBqh3.jpeg?fl=cro,0,0,798,450%7Cres,1200,,1%7Cjpg,80,,1"
+
+//Manipulace s stylem u DOM uzlů
+let teloStranky = document.body;
+teloStranky.style.color = "white";
+teloStranky.style.backgroundColor = "Brown";
