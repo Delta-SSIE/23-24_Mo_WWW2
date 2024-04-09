f1bf2c44bdcde2e9e7b6569ca3ef90cf6a2b585e
Jan Mottl <mottja@delta-skola.cz>
2024-04-09 15:33:03 +0200

    kk

--- 2AS2/JS_Web/dom.js
+++ 2AS2/JS_Web/dom.js
@@ -18,3 +18,17 @@ let selektor1 = document.querySelector('section p');
 console.log(selektor1);
 let selektor2 = document.querySelectorAll('section p');
 console.log(selektor2);
+
+//Manipulace s atributy v uzlu DOM
+let obrazek = document.querySelector('img');
+obrazek.src = "https://d15-a.sdn.cz/d_15/c_img_F_E/6blBqh3.jpeg?fl=cro,0,0,798,450%7Cres,1200,,1%7Cjpg,80,,1";
+selektor1.style = "color: red";
+selektor1.style.color = "green";
+
+//Manpulace s stylem
+let telo = document.body;
+telo.style.backgroundColor = "yellow";
+
+//innerHTML
+selektor1.innerHTML = "Tohle je nový text odstavce!";
+selektor1.innerHTML += " Tohle se připíše k odstavci!";
