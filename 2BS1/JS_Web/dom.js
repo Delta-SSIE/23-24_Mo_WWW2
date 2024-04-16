27838089b9a8eb78237f5e7033441cd85ef61b7c
Jan Mottl <mottja@delta-skola.cz>
2024-04-16 10:07:05 +0200

    kk

--- 2BS1/JS_Web/dom.js
+++ 2BS1/JS_Web/dom.js
@@ -15,7 +15,7 @@ odstavec[1].style.backgroundColor = "blue";
 let p = document.getElementsByClassName('odsta');
 console.log(p);
 //Přidání třídy danému prvku
-p[2].className = "modra";
+p[1].className = "modra";
 
 //Query selektor, který vybírá první prvek z třídy "odsta"
 let odstav = document.querySelector('.odsta')
@@ -33,3 +33,30 @@ obrazek.src = "https://upload.wikimedia.org/wikipedia/commons/thumb/0/09/%D0%90p
 //Výběr body HTML stránky a manipulace s ní
 let teloStranky = document.body;
 teloStranky.style.fontFamily = "Arial";
+
+//Přidání odstavce pomocí innerHTML
+let obalka = document.getElementById('box');
+obalka.innerHTML += "<p> 1. odstavec<p> ";
+obalka.innerHTML += "<p> 2. odstavec<p> ";
+
+for(let i = 3; i < 10;i++){
+    obalka.innerHTML += `<p> ${i}. odstavec<p> `;
+}
+
+
+let box = document.getElementById('box2');
+//vytvoření nového elementu img a zadaní jeho atributů
+let newImg = document.createElement('img');
+newImg.src = "https://www.delta-skola.cz/static/delta/img/logo.png";
+//Připojení prvku newImg jako prvku podřazeného rodičovského prvku box;
+box.appendChild(newImg);
+//Vytvooření nového elementu p
+let newPar = document.createElement('p');
+newPar.innerHTML = "Ahoooj!";
+//přidání prvku newPar pod nadřazený prvek box
+box.appendChild(newPar);
+
+//Vytvoření textového prvku, který se přiřadí jako děstký prvek k prvku odstavce
+let text = document.createTextNode("čus");
+newPar.appendChild(text);
+box.appendChild(newPar);
